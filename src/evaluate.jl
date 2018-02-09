ba9f7f3329a78399e17e41cd2eb9100091c88d04
Sascha Timme <sascha.timme@gmail.com>
2018-02-09 20:41:12 +0100

    Correct output type for evaluate

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -1,12 +1,11 @@
 export evaluate
 
-function evaluate_impl(f::Type{Polynomial{T, NVars, NTerms, Val{Exponents}}}) where {T, NVars, NTerms, Exponents}
-    M = convert_to_matrix(NVars, NTerms, Exponents)
-
+function evaluate_impl(::Type{T}, M::Matrix{Int}) where T
+    m, n = size(M)
     grouped_powers = group_powers(M)
 
     as = [:(out = zero($T))]
-    for j = 1:NTerms
+    for j = 1:n
         factors = []
         push!(factors, :(f.coefficients[$j]))
         nmultiplications = sum(M[:, j] .> 0)
@@ -14,7 +13,7 @@ function evaluate_impl(f::Type{Polynomial{T, NVars, NTerms, Val{Exponents}}}) wh
         if nmultiplications == 0
             push!(as, :(@inbounds out += $(factors[1])))
         else
-            for i=1:NVars
+            for i=1:m
                 k = M[i, j]
                 if k > 0
                     mult_counter += 1
@@ -36,8 +35,13 @@ function evaluate_impl(f::Type{Polynomial{T, NVars, NTerms, Val{Exponents}}}) wh
         :(out))
 end
 
-@generated function evaluate(f::Polynomial{T, NVars, NTerms, Val{Exponents}}, x) where {T, NVars, NTerms, Exponents}
-    evaluate_impl(f)
+function evaluate_impl(::Type{T}, f::Type{Polynomial{S, NVars, NTerms, Val{Exponents}}}) where {T, S, NVars, NTerms, Exponents}
+    M = convert_to_matrix(NVars, NTerms, Exponents)
+    evaluate_impl(promote_type(T, S), M)
+end
+
+@generated function evaluate(f::Polynomial{S, NVars, NTerms, Val{Exponents}}, x::AbstractVector{T}) where {T, S, NVars, NTerms, Exponents}
+    evaluate_impl(T, f)
 end
 
 @inline (f::Polynomial)(x) = evaluate(f, x)
